13c93977485de881fb5b2f8a03f008bc97e90e3f
kacpergoluchowski <kgoluchowski112@gmail.com>
2024-03-20 19:46:33 +0100

    offer done

--- moto-truck_client/src/pages/HomePage.jsx
+++ moto-truck_client/src/pages/HomePage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import '../app.scss';
 import HomePageHero from "../layouts/HomePageHero";
-import HomePageAbout from "../layouts/HomePageAbout";
-
+import HomePageOffer from "../layouts/HomePageOffer";
+ 
 export default function HomePage() {
+
+    window.addEventListener('resize', () => {
+        console.log('width: ', window.innerWidth)
+        console.log('height: ', window.innerHeight)
+    })
+
     return (
         <div className="home_page">
             <HomePageHero />
-            <HomePageAbout />
+            <HomePageOffer />
         </div>
     )
 }
